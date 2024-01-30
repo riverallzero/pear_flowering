f93a155f4cc8b4cb1c176b3397f307f2490edaff
riverallzero <riverallzero@naver.com>
2024-01-30 15:54:05 +0900

    add location find function

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pear_flowering',
-    version='0.1.0',
+    version='0.2.0',
     description='pear-flowering date prediction using DVR, mDVR, CD model',
     url='https://github.com/riverallzero/pear_flowering',
     author='riverallzero',
